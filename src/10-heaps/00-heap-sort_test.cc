8a9c4f706083a9305a5e2d0133f99a2305a92fb9
Ashish Misra <developer.misra@gmail.com>
2022-06-05 12:10:00 -0700

    Added couple of more basic tests for heap_sort

--- src/10-heaps/00-heap-sort_test.cc
+++ src/10-heaps/00-heap-sort_test.cc
@@ -3,10 +3,19 @@
 #include "00-heap-sort.h"
 
 TEST(HeapSort, BasicTest) {
-	std::vector<int> values{10, 1, 300, 24, 5, 0};
-	heap_sort(values);
-
-	EXPECT_TRUE(std::is_sorted(values.begin(), values.end()));
+	// 1. Basic sort test.
+	std::vector<int> values1{10, 1, 300, 24, 5, 0};
+	heap_sort(values1);
+	EXPECT_TRUE(std::is_sorted(values1.begin(), values1.end()));
 	
+	// 2. Empty vector.
+	std::vector<int> values2;
+	heap_sort(values2);
+	EXPECT_TRUE(std::is_sorted(values2.begin(), values2.end()));
+
+	// 3. Already sorted vector.
+	std::vector<int> values3{1, 2, 3};
+	heap_sort(values3);
+	EXPECT_TRUE(std::is_sorted(values3.begin(), values3.end()));
 }
 
