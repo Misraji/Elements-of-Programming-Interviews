72b1fa3b78026953551aee60c194d752ad60b6c6
Ashish Misra <developer.misra@gmail.com>
2022-06-11 18:51:26 -0700

    Implement binary search tree

---
+++ src/14-bst/00-bst.h
@@ -0,0 +1,189 @@
+#ifndef BST_00_BST_H
+#define BST_00_BST_H
+
+#include <memory>
+
+template<typename T>
+class BinarySearchTree {
+	public:
+		struct Node;
+		typedef std::shared_ptr<Node> NodePtr;
+
+		struct Node {
+			T key;
+			NodePtr left, right;	
+
+			void Clear();
+		};
+
+		BinarySearchTree(void) {} 
+		~BinarySearchTree() {
+			Clear();
+		}
+
+		bool Empty() const {
+			return root_ == nullptr;
+		}
+
+		void Clear();
+
+		bool Find(const T& key) const;
+
+		bool Insert(const T& key);
+
+		bool Erase(const T& key);
+
+	private:
+		NodePtr root_;
+
+};
+
+template<typename T>
+void BinarySearchTree<T>::Clear() {
+	if (root_) {
+		root_->Clear();
+		root_ = nullptr;
+	}
+}
+
+template<typename T>
+void BinarySearchTree<T>::Node::Clear() {
+	if (left) {
+		left->Clear();
+		left = nullptr;
+	}
+	if (right) {
+		right->Clear();
+		right = nullptr;
+	}
+}
+
+template<typename T>
+bool BinarySearchTree<T>::Find(const T& key) const {
+	auto current = root_;
+	while (current && current->key != key) {
+		if (current->key > key) {
+			current = current->left;
+
+		} else {
+			current = current->right;
+		}
+	}
+	return current != nullptr;
+}
+
+template<typename T>
+bool BinarySearchTree<T>::Insert(const T& key) {
+	auto node = std::make_shared<Node>();
+	node->key = key;
+
+	if (!root_) {
+		root_ = node;
+		return true;
+	}
+
+	auto current = root_;
+	NodePtr prev = nullptr;
+
+	while (current) {
+		prev = current;
+
+		if (key == prev->key) {
+			return false;
+
+		} else if (key > current->key) { 
+			current = current->right;
+
+		} else {
+			current = current->left;
+		}
+	}
+
+	if (key > prev->key) {
+		prev->right = node;
+
+	} else {
+		prev->left = node;
+	}
+	return true;
+}
+
+template<typename T>
+bool BinarySearchTree<T>::Erase(const T& key) {
+
+	if (!root_) {
+		return false;
+	}
+
+	NodePtr current = root_;
+	NodePtr prev;
+
+	while (current && current->key != key) {
+		prev = current;
+		if (current->key > key) {
+			current = current->left;
+
+		} else {
+			current = current->right;
+		}
+	}
+
+	// Element not found.
+	if (!current) {
+		return false;
+	}
+
+	auto target_node = current;
+
+	if (current->left) {
+
+		prev = current;
+		current = current->left;
+
+		while(current->right) {
+			prev = current;
+			current = current->right;
+		}
+
+		std::swap(target_node->key, current->key);
+
+	} else if (current->right) {
+
+		prev = current;
+		current = current->right;
+
+		while (current->left) {
+			prev = current;
+			current = current->left;
+		}
+
+		std::swap(target_node->key, current->key);
+
+	} else {
+		// current node is a leaf. Do nothing.
+	}
+
+	if (!prev) {
+		// We are deleting the root.
+		root_ = nullptr;
+
+	} else if (prev->left == current) {
+		prev->left = nullptr;
+
+	} else {
+		prev->right = nullptr;
+	}
+
+	return true;
+}
+
+template<typename T>
+std::unique_ptr<BinarySearchTree<T>> Create(const std::vector<T> &keys) {
+	auto bst = std::unique_ptr<BinarySearchTree<T>>(new BinarySearchTree<T>());
+	for(const auto& key : keys) {
+		bst->Insert(key);
+	}
+	return bst;
+}
+
+#endif  // BST_00_BST_H
---
+++ src/14-bst/00-bst_test.cc
@@ -0,0 +1,67 @@
+#include <gtest/gtest.h>
+
+#include "00-bst.h"
+
+TEST(BinarySearchTree, EmptyTest) {
+	BinarySearchTree<int> bst;
+
+	EXPECT_TRUE(bst.Empty());
+	EXPECT_FALSE(bst.Find(5));
+	EXPECT_FALSE(bst.Erase(5));
+
+	bst.Clear();
+}
+
+TEST(BinarySearchTree, SingleValueTest) {
+	auto bst = Create(std::vector<int>({1}));
+
+	EXPECT_FALSE(bst->Empty());
+	EXPECT_TRUE(bst->Find(1));
+	EXPECT_FALSE(bst->Find(5));
+
+	EXPECT_TRUE(bst->Erase(1));
+	EXPECT_FALSE(bst->Find(1));
+	EXPECT_TRUE(bst->Empty());
+
+	EXPECT_TRUE(bst->Insert(1));
+	bst->Clear();
+	EXPECT_TRUE(bst->Empty());
+}
+
+TEST(BinarySearchTree, TwoValuesTest) {
+	auto bst = Create(std::vector<int>({1, 2}));
+
+	EXPECT_FALSE(bst->Empty());
+	EXPECT_TRUE(bst->Find(1));
+	EXPECT_TRUE(bst->Find(2));
+	EXPECT_FALSE(bst->Find(5));
+
+	EXPECT_TRUE(bst->Erase(1));
+	EXPECT_FALSE(bst->Find(1));
+	EXPECT_FALSE(bst->Empty());
+
+	EXPECT_TRUE(bst->Find(2));
+	EXPECT_TRUE(bst->Erase(2));
+	EXPECT_TRUE(bst->Empty());
+
+	bst->Insert(2);
+	bst->Insert(1);
+	
+	EXPECT_TRUE(bst->Erase(1));
+	EXPECT_FALSE(bst->Find(1));
+	EXPECT_FALSE(bst->Empty());
+
+	EXPECT_TRUE(bst->Find(2));
+	EXPECT_TRUE(bst->Erase(2));
+	EXPECT_TRUE(bst->Empty());
+}
+
+TEST(BinarySearchTree, BalancedTreeTest) {
+
+	auto bst = Create(std::vector<int>({2, 1, 3}));
+
+	EXPECT_TRUE(bst->Erase(2));
+	EXPECT_TRUE(bst->Erase(3));
+	EXPECT_TRUE(bst->Erase(1));
+}
+
